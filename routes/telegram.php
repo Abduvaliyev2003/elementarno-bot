33ae6bd5181e052f531376992ba025126509e9ed
Abduvaliyev2003 <abduvaliyevasad23@gmail.com>
2024-05-18 18:06:14 +0500

    first step

---
+++ routes/telegram.php
@@ -0,0 +1,17 @@
+<?php
+/** @var SergiX44\Nutgram\Nutgram $bot */
+
+use App\TelegramBot\Commands\StartCommand;
+use SergiX44\Nutgram\Nutgram;
+
+/*
+|--------------------------------------------------------------------------
+| Nutgram Handlers
+|--------------------------------------------------------------------------
+|
+| Here is where you can register telegram handlers for Nutgram. These
+| handlers are loaded by the NutgramServiceProvider. Enjoy!
+|
+*/
+
+$bot->registerCommand(StartCommand::class);
