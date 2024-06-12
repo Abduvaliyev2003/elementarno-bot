0f9a8cc688392ab0116f76b4e0d58f7d69d7d42d
Abduvaliyev2003 <abduvaliyevasad23@gmail.com>
2024-06-12 23:46:18 +0500

    change

---
+++ app/Http/Controllers/WordCategoriesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreWordCategoriesRequest;
+use App\Http\Requests\UpdateWordCategoriesRequest;
+use App\Models\WordCategories;
+
+class WordCategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreWordCategoriesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WordCategories $wordCategories)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(WordCategories $wordCategories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateWordCategoriesRequest $request, WordCategories $wordCategories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WordCategories $wordCategories)
+    {
+        //
+    }
+}
